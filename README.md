bd1436ea437e1b0f96cf6707b3e7322215d87e2e
Steve Regester <steve@studylog.com>
2022-04-30 19:25:08 -0700

    Add readme, documentation

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hey Candie
+
+This repo is a place for us to share code back and forth.
+I will add code here demonstrating different concepts.
+You can add stuff here, too. I will demonstrate how at some point.
--- index.js
+++ index.js
@@ -1,33 +1,48 @@
-/* Control Flow Mechanisms */
+const readMeFile = require('path').join(__dirname, 'README.md')
+const readMe = require('fs').readFileSync(readMeFile, 'utf-8')
+console.log(readMe)
 
-// If statements
-if (true)  console.log('if (true)')
-if (false) console.log('if (false)')
+console.log('# Control Flow Mechanisms')
 
-// If/Else statements
-if (true) console.log('if/else (true)')
-else      console.log('if/else (else)')
+console.log('## if statements')
+console.log()
+if (true)  console.log(' - if (true)')
+if (false) console.log(' - if (false)')
+console.log()
 
-// if/else/if statements
-if (true)                     console.log('if/else/if (true)')
-else if ('andie' === 'steve') console.log('if/else/if (anbdie === steve)')
-else                          console.log('if/else/if (else)')
+console.log('## if/else statements')
+console.log()
+if (true) console.log(' - if/else (true)')
+else      console.log(' - if/else (else)')
+console.log()
 
-// switch/case/default
+console.log('## if/else/if statements')
+console.log()
+if (true)                     console.log(' - if/else/if (true)')
+else if ('andie' === 'steve') console.log(' - if/else/if (anbdie === steve)')
+else                          console.log(' - if/else/if (else)')
+console.log()
+
+console.log('## switch/case/default')
+console.log()
 switch ('steve') {
-  case 'andie': console.log('switch/case (andie)'); break;
-  case 'steve': console.log('switch/case (steve)'); break;
-  default: console.log('switch/case (default)'); break;
+  case 'andie': console.log(' - switch/case (andie)'); break;
+  case 'steve': console.log(' - switch/case (steve)'); break;
+  default:      console.log(' - switch/case (default)'); break;
 }
+console.log()
 
-// for loop
-const cycles = 0
-
+const cycles = 10
+console.log('## for loop', { cycles })
+console.log()
 for (let i = 0; i < cycles; i++) {
-  console.log('for loop', i)
+  console.log(' - for loop', i)
 }
+console.log()
 
-// do.while loop
+console.log('## do/while loop', { cycles })
+console.log()
 let i = 0;
-do { console.log('do/while', i++) }
+do { console.log(' - do/while', i++) }
 while (i < cycles)
+console.log()
